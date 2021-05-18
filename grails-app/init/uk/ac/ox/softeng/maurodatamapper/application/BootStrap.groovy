7f5bf6871b394ef49e2e12be35529d71295dbcf6
Adei Josol <helena.josol@bdi.ox.ac.uk>
2021-05-18 13:51:07 +0100

    gh-1 Create folder, add security to admin group

--- grails-app/init/uk/ac/ox/softeng/maurodatamapper/application/BootStrap.groovy
+++ grails-app/init/uk/ac/ox/softeng/maurodatamapper/application/BootStrap.groovy
@@ -17,16 +17,60 @@
  */
 package uk.ac.ox.softeng.maurodatamapper.application
 
+import uk.ac.ox.softeng.maurodatamapper.core.container.Folder
+import uk.ac.ox.softeng.maurodatamapper.security.CatalogueUser
+import uk.ac.ox.softeng.maurodatamapper.security.UserGroup
+import uk.ac.ox.softeng.maurodatamapper.security.role.GroupRole
+import uk.ac.ox.softeng.maurodatamapper.security.role.GroupRoleService
+import uk.ac.ox.softeng.maurodatamapper.security.role.SecurableResourceGroupRole
 import uk.ac.ox.softeng.maurodatamapper.security.utils.SecurityDefinition
 
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.MessageSource
+
+import static uk.ac.ox.softeng.maurodatamapper.util.GormUtils.checkAndSave
 
 @Slf4j
 class BootStrap implements SecurityDefinition {
 
+    @Autowired
+    MessageSource messageSource
+
+    GroupRoleService groupRoleService
+
     def init = { servletContext ->
+
+        log.debug('Main bootstrap complete')
+
         environments {
             production {
+                CatalogueUser.withNewTransaction {
+
+                    admins = UserGroup.findByName('administrators')
+                    if (!admins) {
+                        createAdminGroup('admin')
+                        checkAndSave(messageSource, admins)
+                    }
+
+                    Folder folder = new Folder(
+                        label: 'Example Folder',
+                        createdBy: userEmailAddresses.production,
+                        readableByAuthenticatedUsers: true,
+                        description: 'This folder is readable by all authenticated users, and currently only editable by users in the ' +
+                                     'administrators group. Future suggestions: rename this folder and alter group access.')
+                    checkAndSave(messageSource, folder)
+
+                    if (SecurableResourceGroupRole.bySecurableResourceAndGroupRoleIdAndUserGroupId(
+                        folder, groupRoleService.getFromCache(GroupRole.CONTAINER_ADMIN_ROLE_NAME).groupRole.id, admins.id).count() == 0) {
+                        checkAndSave(messageSource, new SecurableResourceGroupRole(
+                            createdBy: userEmailAddresses.production,
+                            securableResource: folder,
+                            userGroup: admins,
+                            groupRole: groupRoleService.getFromCache(GroupRole.CONTAINER_ADMIN_ROLE_NAME).groupRole))
+                    }
+                }
+                log.debug('Production environment bootstrap complete')
             }
         }
     }
